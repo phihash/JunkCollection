cda67ed02cb9c3403fd1bc141a2645f7f4893aad
Mail I <maili@merunoMacBook-Air.local>
2021-12-01 01:16:20 +0900

    tab切り替え実装

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 $(function(){
-  $('.tab-btn').click(function(){
+
+  $(document).on("click", ".tab-btn", function() {
+    // clickイベントの処理
     var index = $('.tab-btn').index($(this));
 
     $('.active').removeClass('active');
 
     $('.tab-panel').eq(index).addClass('active');
-  })
+  });
+
 })
